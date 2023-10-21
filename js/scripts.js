b302f7e502e5f97d5a91e22a8cf3d440b00241ff
developer-gilberto <developer.gilberto@gmail.com>
2023-10-20 22:41:13 -0300

    att style obrigado.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,12 @@
-const nav = document.querySelector('#nav')
+const navMenuMobile = document.querySelector('#nav')
 const itensMenu = document.querySelectorAll('#nav a')
 
 function abreFechaMenu(){
-    nav.classList.toggle('showNav')
+    navMenuMobile.classList.toggle('showNav')
 }
 
 itensMenu.forEach((item) => {
     item.addEventListener('click', () => {
         abreFechaMenu()
     })
-})
-
-const btnTop = document.querySelector('#btn-arrow')
-
-window.addEventListener('scroll', () => {
-    if(scrollY >= 900){
-        btnTop.style.display = 'block'
-    } else {
-        btnTop.style.display = 'none'
-    }
 })
